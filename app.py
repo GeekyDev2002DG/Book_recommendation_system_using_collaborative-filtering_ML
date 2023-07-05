58d4eb6d46bbfdc54bc9e13a8b44ee3994455d45
DEVPARNO GHOSAL <72439194+GeekyDev2002DG@users.noreply.github.com>
2023-07-06 00:38:13 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 import numpy as np
 
 
-st.header('Book Recommender System Using Machine Learning')
+st.header('Book Recommender App using Collaborative Filtering')
 model = pickle.load(open('artifacts/model.pkl','rb'))
 book_names = pickle.load(open('artifacts/book_names.pkl','rb'))
 final_rating = pickle.load(open('artifacts/final_rating.pkl','rb'))
